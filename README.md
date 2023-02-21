270282d73d7fc8cff0bd1ff91401640d74a7280e
Timo Glastra <timo@animo.id>
2023-02-21 18:54:37 +0100

    feat: initial commit
    Signed-off-by: Timo Glastra <timo@animo.id>

---
+++ README.md
@@ -0,0 +1,23 @@
+# Aries Framework JavaScript Example
+
+This example demonstrates how to use the Aries Framework JavaScript to create a simple agent that can connect to other agents and exchange messages.
+
+## Getting Started
+
+Make sure you have Node.JS 18+ & Yarn installed.
+
+```
+git clone https://github.com/TimoGlastra/aries-framework-javascript-example.git
+
+cd aries-framework-javascript-example
+
+yarn install
+```
+
+## Running
+
+After running you should see a QR code in the terminal. This is a shortened out of band v1 invitation url, and can be scanned with a mobile agent.
+
+```
+yarn start
+```
---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import { connect } from 'ngrok'
+import { agent } from './agent'
+import * as QRCode from 'qrcode'
+import { server } from './server'
+import { OutOfBandState } from '@aries-framework/core'
+
+async function app() {
+  await agent.initialize()
+  agent.config.logger.info('Agent initialized!')
+
+  // expose agent port to ngrok and set the agent to use the ngrok url
+  const ngrokUrl = await connect(6006)
+  agent.config.endpoints = [ngrokUrl]
+
+  server.get('/s/:invitationId', async (req, res) => {
+    const outOfBandRecord = await agent.oob.findByCreatedInvitationId(req.params.invitationId)
+
+    if (!outOfBandRecord || outOfBandRecord.state !== OutOfBandState.AwaitResponse)
+      return res.status(404).send('Not found')
+
+    const invitationJson = outOfBandRecord.outOfBandInvitation.toJSON({ useDidSovPrefixWhereAllowed: true })
+
+    return res.header('Content-Type', 'application/json').send(invitationJson)
+  })
+
+  // Create out of band invitation
+  const { outOfBandInvitation } = await agent.oob.createInvitation()
+
+  // Create short url for invitation
+  const shortUrl = `${ngrokUrl}/s/${outOfBandInvitation.id}`
+  const qrInvitation = await QRCode.toString(shortUrl, { type: 'terminal' })
+
+  console.log(qrInvitation)
+}
+
+app()
